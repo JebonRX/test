53f988faa14379487559d773aace402b71f1f1d0
Nevermore <153726581+JebonRX@users.noreply.github.com>
2025-12-09 21:25:06 +0800

    Update add-ssh.sh

--- ssh/add-ssh.sh
+++ ssh/add-ssh.sh
@@ -6,30 +6,43 @@
 # (C) Copyright 2025 - 2026
 # =========================================
 
-# public ip
+# Warna
+line="38;5;208"         # Oyen terang
+GREEN="\e[92m" # hijau
+PINK="\e[38;5;205m" # Pink terang
+back_text="1;37;44"  # Putih + biru gelap
+box="1;37"           # Putih bold
+# ============================
+# COLOR THEME PREMIUM
+# ============================
+text="1;37"          # Putih bold (info text)
+title="\e[30;107m"   # 30 = hitam, 107 = background putih
+number="\e[38;5;205"        # Kuning gold (untuk nombor menu)
+below="0;37"         # Putih lembut
+reset="\e[0m"
+
+# Public IP
 MYIP=$(curl -s ipv4.icanhazip.com || curl -s ipinfo.io/ip || curl -s ifconfig.me)
+domain=$(cat /usr/local/etc/xray/domain)
+IP=$(curl -s ipv4.icanhazip.com || curl -s ipinfo.io/ip || curl -s ifconfig.me);
+DIR="/etc/xray/config"
 clear
 
 clear
-echo -e   "  \e[$line═══════════════════════════════════════════════════════\e[m"
-echo -e   "  \e[$back_text           \e[30m[\e[$box CREATE USER SSH & OPENVPN\e[30m ]\e[1m               \e[m"
-echo -e   "  \e[$line═══════════════════════════════════════════════════════\e[m"
+echo ""
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
+echo -e "  \e[${title}[ CREATE USER • SSH WEBSOCKET ]${reset}"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
+echo ""
 read -p "   Username : " Login
 read -p "   Password : " Pass
 read -p "   Expired (days): " masaaktif
 
-IP=$(curl -s ipv4.icanhazip.com || curl -s ipinfo.io/ip || curl -s ifconfig.me);
-if [[ "$IP" = "" ]]; then
-domain=$(cat /usr/local/etc/xray/domain)
-else
-domain=$IP
-fi
-
 export ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
 export wsdropbear="$(cat ~/log-install.txt | grep -w "Websocket HTTP" | cut -d: -f2|sed 's/ //g')"
 export wsstunnel="$(cat ~/log-install.txt | grep -w "Websocket HTTPS" | cut -d: -f2|sed 's/ //g')"
-nsdomain1=$(cat /root/nsdomain)
-pubkey1=$(cat /etc/slowdns/server.pub)
+#nsdomain1=$(cat /root/nsdomain)
+#pubkey1=$(cat /etc/slowdns/server.pub)
 
 sleep 1
 echo Create Premium SSH Config
@@ -46,6 +59,15 @@ export harini=`date -d "0 days" +"%Y-%m-%d"`
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 export exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 export exp1=`date -d "$masaaktif days" +"%Y-%m-%d"`
+
+# Check if the folder exists
+if [ -d "$DIR" ]; then
+    echo "Folder $DIR already exists. Skipping..."
+else
+    echo "Folder $DIR does not exist. Creating folder..."
+    mkdir -p "$DIR"
+    echo "Folder $DIR has been created successfully."
+fi
 
 cat >  /etc/xray/config/ssh-$Login.txt <<-END
 ====================================================================
@@ -93,40 +115,43 @@ PAYLOAD WSS      : GET wss://$sni/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: web
 END
 
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+clear
 echo -e ""
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
-echo -e "\e[$back_text         \e[30m[\e[$box Premium Account SSH & OpenVPN\e[30m ]\e[1m           \e[m"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+# OUTPUT
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
+echo -e "  \e[${line}m[ PREMIUM SERVER SSH WEBSOCKET ]\e[${reset}"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo -e "Username         : $Login"
 echo -e "Password         : $Pass"
 echo -e "Created          : $harini"
 echo -e "Expired          : $exp1"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo -e "Domain           : $domain"
-echo -e "Name Server(NS)  : $nsdomain1"
-echo -e "Pubkey           : $pubkey1"
+#echo -e "Name Server(NS)  : $nsdomain1"
+#echo -e "Pubkey           : $pubkey1"
 echo -e "IP/Host          : $MYIP"
 echo -e "OpenSSH          : 22"
 echo -e "Dropbear         : 143, 109"
 echo -e "SSL/TLS          :$ssl"
-echo -e "SlowDNS          : 22,80,443,53,5300"
-echo -e "SSH-UDP          : 1-65535"
+#echo -e "SlowDNS          : 22,80,443,53,5300"
+#echo -e "SSH-UDP          : 1-65535"
 echo -e "WS SSH(HTTP)     : $wsdropbear"
 echo -e "WS SSL(HTTPS)    : $wsstunnel"
 echo -e "Badvpn(UDPGW)    : 7100-7300"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo -e "CONFIG SSH WS"
-echo -e "SSH Config  : http://${domain}:81/ssh-$Login.txt"
+echo "---------------"
+#echo -e "SSH Config  : http://${domain}:81/ssh-$Login.txt"
 echo -e "SSH 22      : $(cat /usr/local/etc/xray/domain):22@$Login:$Pass"
 echo -e "SSH 80      : $(cat /usr/local/etc/xray/domain):80@$Login:$Pass"
 echo -e "SSH 443     : $(cat /usr/local/etc/xray/domain):443@$Login:$Pass"
-echo -e "UDP-CUSTOM  : $(cat /usr/local/etc/xray/domain):1-65535@$Login:$Pass"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+#echo -e "UDP-CUSTOM  : $(cat /usr/local/etc/xray/domain):1-65535@$Login:$Pass"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
+#echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo -e "PAYLOAD WS       : GET / HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo -e "PAYLOAD WSS      : GET wss://$sni/ HTTP/1.1[crlf]Host: $domain[crlf]Upgrade: websocket[crlf]Connection: Keep-Alive[crlf][crlf]"
-echo -e "\e[$line═══════════════════════════════════════════════════════\e[m"
+echo -e "\e[${line}m════════════════════════════════════════════════════${reset}"
 echo ""
 read -n 1 -s -r -p "Press any key to back on menu SSH"
 exec menu-ssh
