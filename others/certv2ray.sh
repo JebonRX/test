2dfcdc34e67e7e490b0c01a57f1ea252bda43c8b
Nevermore <153726581+JebonRX@users.noreply.github.com>
2025-12-10 23:01:19 +0800

    Create certv2ray.sh

---
+++ others/certv2ray.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+# =========================================
+# Renew Cert Xrays
+# Author: NevermoreSSH
+# Date: 2025-11-29
+# =========================================
+
+# color 
+red='\e[1;31m'
+green='\e[0;32m'
+purple='\e[0;35m'
+orange='\e[0;33m'
+NC='\e[0m'
+clear
+
+# public ip
+MYIP=$(curl -s ipv4.icanhazip.com || curl -s ipinfo.io/ip || curl -s ifconfig.me)
+IP=$(curl -s ipv4.icanhazip.com || curl -s ipinfo.io/ip || curl -s ifconfig.me)
+date=$(date +"%Y-%m-%d-%H:%M:%S")
+domain=$(cat /usr/local/etc/xray/domain)
+clear
+echo start
+sleep 0.5
+source /var/lib/premium-script/ipvps.conf
+domain=$(cat /usr/local/etc/xray/domain)
+emailcf=$(cat /usr/local/etc/xray/email)
+clear
+systemctl stop xray
+echo -e "\e[0;32mStart renew your Certificate SSL\e[0m"
+sleep 1
+/root/.acme.sh/acme.sh --upgrade --auto-upgrade
+/root/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+/root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /usr/local/etc/xray/xray.crt --keypath /usr/local/etc/xray/xray.key --ecc
+systemctl start xray
+echo Done
+sleep 0.5
+#echo -e "\e[0;32m All finished ! \e[0m" 
+echo "✅ Success Renew Cert XRAYS!"
+sleep 1
+clear
