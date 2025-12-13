7c5f3c6b48e289553f99f5177e685217cfe7dfa6
Nevermore <153726581+JebonRX@users.noreply.github.com>
2025-12-13 23:45:20 +0800

    Update ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -79,7 +79,11 @@ ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 
 # install req packages
-apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+#apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+apt-get install -y \
+bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools \
+zip unzip curl nano sed gnupg gnupg1 bc apt-transport-https \
+build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
 echo "menu" >> .profile
 
